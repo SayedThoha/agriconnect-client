bc029c69c13e4b8d8fb68edf8fb0e31d5d30ab3e
SayedThoha <147703696+SayedThoha@users.noreply.github.com>
2025-02-19 19:15:09 +0530

    changed api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000'  // Your local or development API URL
+    // apiUrl: 'http://localhost:3000'  // Your local or development API URL
+    apiUrl: 'https://www.agriconnect.club' 
+
   };

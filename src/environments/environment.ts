b5c6f0b0d34f3d5cf06891fd882bdf79d4c46f3c
SayedThoha <147703696+SayedThoha@users.noreply.github.com>
2025-02-19 21:13:02 +0530

    bug fixes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    // apiUrl: 'http://localhost:3000'  // Your local or development API URL
-    apiUrl: 'https://www.agriconnect.club' 
+    apiUrl: 'http://localhost:3000'  // Your local or development API URL
+    // apiUrl: 'https://www.agriconnect.club' 
 
   };
